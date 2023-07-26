a96d7687b3e80c9bca7280679b921668eb758406
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-07-26 12:18:33 +0300

    Form handling script, form icons
    Change form icons to one more apropriate
    
    Add script that handles form reaction on input and
    content display

--- scripts/script.js
+++ scripts/script.js
@@ -18,18 +18,133 @@ const cellNodeList = document.getElementsByClassName('cell');
 const formScreen = document.getElementById('form-container');
 // collecting form
 const playerForm = document.getElementById('start');
-// cpu type radio for player 'x'
-const cpuRadioX = document.getElementById('computer-x')
-// select element for player 'x'
-const cpuDifX = document.getElementById('cpu-dif-x');
-// cpu type radio for player 'o'
-const cpuRadioO = document.getElementById('computer-o')
-// select element for player 'o'
-const cpuDifO = document.getElementById('cpu-dif-o')
 
 // game end message screen
 const endScreen = document.getElementById('end-screen');
 // game end message
-const endMessage = document.getElementById('end-message')
+const endMessage = document.getElementById('end-message');
 // replay button
 const reButton = document.getElementById('ret-button');
+
+// function for collecting player data
+
+// 1.Pushing the 'start' button will call the form to collect player data
+
+// 2.Script will create player objects according to the data
+
+// 3.Script initiates the array, containing game field data, hides the start button and gives cells class '.active'
+
+// 4.Turn 'x', player annonsed, array checked, gamefield refreshed, waiting for player input, input taken, array modified, array cheked for winning conditions
+
+// 5.Turn 'o', player annonsed, array checked, gamefield refreshed, waiting for player input, input taken, array modified, array cheked for winning conditions
+
+// 6.Repeat turns until array matches winning conditions, stop game, announce the result
+
+// 7.'Try again' button reloads page
+
+// Script to maintain the form
+
+const form = (() => {
+    const formRadioInputs = document.querySelectorAll('input[type="radio"]');
+    const formRadioLabels = document.querySelectorAll('.radio')
+    const formSelectInputX = document.getElementById('cpu-dif-x');
+    const formSelectLabelX = document.querySelector('label[for="cpu-dif-x"]');
+    const formSelectInputO = document.getElementById('cpu-dif-o');
+    const formSelectLabelO = document.querySelector('label[for="cpu-dif-o"]');
+
+    const updateRadios = () => {
+        const show = (item) => {
+            item.classList.remove('no-visible')
+        };
+        const hide = (item) => {
+            item.classList.add('no-visible')
+        };
+        const check = (item) => {
+            item.classList.add('radio-checked')
+        };
+        const unCheck = (item) => {
+            item.classList.remove('radio-checked')
+        };
+
+        for(let i = 0; i < formRadioInputs.length; i++) {
+            let radio = formRadioInputs[i];
+            let label = formRadioLabels[i];
+
+            if (radio.checked) {
+                check(label);
+                switch(radio.id) {
+                    case 'human-x':
+                        hide(formSelectLabelX);
+                        hide(formSelectInputX);
+                        break;
+                    case 'computer-x':
+                        show(formSelectLabelX);
+                        show(formSelectInputX);
+                        break;
+                    case 'human-o':
+                        hide(formSelectLabelO);
+                        hide(formSelectInputO);
+                        break;
+                    case 'computer-o':
+                        show(formSelectInputO);
+                        show(formSelectLabelO);
+                        break;
+                };
+            } else {
+                unCheck(label);
+            }
+        }; 
+    }
+
+    const clickRadios = (event) => {
+        console.log(event)
+        event.target.click();
+        updateRadios()
+    }
+  
+    const listenRadios = () => {
+        for(i = 0; i < formRadioLabels.length; i++) {
+            let radio = formRadioLabels[i];
+            radio.addEventListener('click', clickRadios);
+            console.log(radio);
+        };
+    };
+
+    const unListenRadios = () => {
+        for(i = 0; i < formRadioLabels.length; i++) {
+            let radio = formRadioLabels[i];
+            radio.removeEventListener('click', clickRadios);
+            console.log(radio);
+        };
+    };
+  
+    // Public methods
+    return {
+      listenRadios: listenRadios,
+      unListenRadios: unListenRadios
+    };
+})();
+
+const toggleVisibility = (item) => {
+    item.classList.toggle('no-visible');
+};
+
+const toggleBlur = () => {
+    gameMain.classList.toggle('blur');
+}
+
+
+const startGame = () => {
+    toggleVisibility(startButton);
+    toggleVisibility(announcer);
+    toggleBlur();
+    toggleVisibility(formScreen);
+    form.listenRadios();
+  };
+
+
+startButton.addEventListener('click', startGame);
+
+
+  
+  
